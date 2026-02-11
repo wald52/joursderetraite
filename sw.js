29b83e6e55578657761496e1745faf04f35185d5
wald52 <wald52@protonmail.com>
2026-02-11 23:48:38 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service worker pour l'application PWA
 
-const CACHE_NAME = 'joursderetraite-v75'; // Mise à jour du numéro de version
+const CACHE_NAME = 'joursderetraite-v76'; // Mise à jour du numéro de version
 const urlsToCache = [
   './',
   'index.html',
